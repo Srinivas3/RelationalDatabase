aa113ee46453f348835633d39fbda488470ad1ed
Rishi <sririshindra@gmail.com>
2019-02-05 18:41:56 -0500

    8th commit

--- src/main/java/dubstep.java
+++ src/main/java/dubstep.java
@@ -6,26 +6,36 @@ import net.sf.jsqlparser.parser.ParseException;
 import net.sf.jsqlparser.schema.Table;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.SelectBody;
 
 public class dubstep {
 
     public static void main(String args[])throws ParseException, FileNotFoundException {
 
+        System.out.println("entered line 14");
         Scanner scan = new Scanner(System.in);
 
-        while (scan.hasNext()){
-            StringReader input = new StringReader(scan.next());
+        while (scan.hasNextLine()){
+            String next = scan.nextLine();
+            System.out.println(next);
+            StringReader input = new StringReader(next);
             CCJSqlParser parser = new CCJSqlParser(input);
             Statement query = parser.Statement();
+            System.out.println(query);
+            if (query instanceof Select){
+                System.out.println("entered line 25");
+                SelectBody body = ((Select) query).getSelectBody();
 
-            if (query instanceof PlainSelect){
-
-                if (((PlainSelect) query).getFromItem() instanceof Table){
-                    Table table = (Table ) ((PlainSelect) query).getFromItem();
+                if (((PlainSelect) body).getFromItem() instanceof Table){
+                    Table table = (Table ) ((PlainSelect) body).getFromItem();
 
                     String tableName = table.getName();
                     try{
-                        BufferedReader reader = new BufferedReader(new FileReader(new File("data/" + tableName)));
+                        File f = new File("data/" + tableName +".csv ");
+//                        System.out.println(f.getAbsolutePath());
+                        BufferedReader reader = new BufferedReader(new FileReader(f.getAbsoluteFile()));
+
                         String line = null;
                         while ((line = reader.readLine()) != null){
                             System.out.println(line);
